5d018b8780b5b2beae9d9e46ddb40498507af8ef
Chirag Shahani <84765953+ChiragShahani@users.noreply.github.com>
2021-06-10 11:27:42 +0530

    Use of Template

---
+++ Day 8/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.homepageview,name="Home"), 
+    path('about',views.aboutpageview,name='about'),
+    path('contact',views.contactpageview,name='contact')
+]
